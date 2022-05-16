611a757d2bb17feb85e8c463d3f3475ba66ccbbe
Pawan Rao <you@example.com>
2022-05-15 22:59:29 -0400

    Fix bugs

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+from matplotlib.pyplot import title
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
 from plotly.offline import plot
+import plotly.express as px 
 from plotly.graph_objs import Scatter
+import plotly.graph_objs as go
+
 from flask import Markup
+import json
+import plotly
 
 
 #Creat flask app
@@ -22,17 +28,22 @@ def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
     prediction = model.predict(features)
+    print(prediction)
     prediction = np.round(prediction[0],1)
 
-    #prediction with 5% error at different age
+    # Single plot age at predicition vs final cobb
     age_last = float_features[4]
+    final_predict = prediction
+
+    #prediction with 5% error at different age
+    
     age_first = float_features[7]
     age_arr = np.arange(np.floor(age_first+1), 19)
     age_arr = np.insert(age_arr,0,age_first)
 
     angle = float_features[0]
     angle_arr = [angle -5, angle, angle+5]
-    new_float_features=float_features
+    new_float_features=float_features.copy()
     pred_angle = []
     for i in angle_arr:
         new_float_features[0]= i
@@ -41,24 +52,41 @@ def predict():
         for a in age_arr:
             new_float_features[4] = a
             features_new = [np.array(new_float_features)]
-            pred_angle.append(model.predict(features_new))
+            pred_angle.append(model.predict(features_new)[0])
+    print(type(pred_angle))
 
     pred_angle1 = pred_angle[0:len(age_arr)] #Cobb -5
     pred_angle2 = pred_angle[len(age_arr): 2*len(age_arr)] #Cobb
     pred_angle3 = pred_angle[2*len(age_arr):] #Cobb +5
-    pred_angle1 = np.insert(pred_angle1,0,angle_arr[0])
-    pred_angle2 = np.insert(pred_angle2,0,angle_arr[1])
-    pred_angle3 = np.insert(pred_angle3,0,angle_arr[2])
-      
-    
-    
-    x_val=features[0]
-    y_val=np.array(prediction)
-    my_plot_div = plot([Scatter(x=age_arr, y=(pred_angle1))], output_type='div')
-    my_plot_div1 = plot([Scatter(x=age_arr, y=(pred_angle2))], output_type='div')    
+    #pred_angle1 = np.insert(pred_angle1,0,angle_arr[0])
+    #pred_angle2 = np.insert(pred_angle2,0,angle_arr[1])
+    #pred_angle3 = np.insert(pred_angle3,0,angle_arr[2])
+    print(age_arr)
+    print(pred_angle2)
+    layout = go.Layout(title = "Cobb Angle prediction from current age to 18 years", xaxis = {'title':'Age (years)'}, yaxis = {'title':'Cobb Angle (degrees)'})  
+    fig = go.Figure(data =[go.Scatter(x = age_arr,
+                                   y = np.round((pred_angle2),1),
+                                   mode ='lines+markers',
+                                   name ='Curve Progression Prediction')], layout = layout)
+  
+    fig.add_trace(go.Scatter(x=np.array(age_last), y=np.array(final_predict), 
+    mode='markers',marker={'color' : 'red'}, name='Current Prediction'))
+    fig.show()
+ 
+
+# 'frames': [{'data': [{'x': [age_last], 'y': [final_predict]}]}]
+   # my_plot_div = plot([Scatter(x=age_arr, y=(pred_angle1))], output_type='div', image_filename="Cobb angle predicition")
+    #my_plot_div.add_trace(go.scatterr(x=age_last, y=final_predict, mode='markers'))
+    #my_plot_div = plot([Scatter(x=[age_arr,age_last], y=[(pred_angle1),final_predict])], output_type='div')
+    #my_plot_div.plot([Scatter(x=age_last, y=final_predict)])
+
+    #my_plot_div.add_scatter(x=age_arr, y=(pred_angle2))
+   #my_plot_div1 = plot([Scatter(x=age_arr, y=(pred_angle2))], output_type='div')    
+
+
 
     return render_template("index.html", 
-        InitialCobb=angle,
+        InitialCobb=float_features[0],
         Flexibility=float_features[1],
         Lordosis=float_features[2],
         Kyphosis=float_features[3],
@@ -67,19 +95,16 @@ def predict():
         Risser=float_features[6],
         AgeFirst=float_features[7],
         Gender=float_features[8],
-        prediction_text = " The Final Cobb angle is {} degrees".format(prediction), 
-        xval=x_val, 
-        yval=y_val, 
+        prediction_text = " The Final Cobb angle is {} degrees".format(prediction),  
         pred_angle1=pred_angle1,
         pred_angle2=pred_angle2,
         pred_angle3=pred_angle3,
         age_arr=age_arr,
-        div_placeholder=Markup(my_plot_div),
-        div_placeholder1=Markup(my_plot_div1))
+        #div_placeholder=Markup(my_fig)
+        #div_placeholder=Markup(my_plot_div)
+    )
 
-
-
-
+        
 
 """""
 age_last = float_features[0,3]
